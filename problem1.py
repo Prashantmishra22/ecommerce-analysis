49a93f350d73ec1103ae47502bfc3166f39ab2f2
Prashantmishra22 <ps956984mishr@gmail.com>
2026-04-21 21:09:57 +0530

    Add files via upload
    Updated full project with final code and fixes

--- problem1.py
+++ problem1.py
@@ -1,30 +1,41 @@
-# Problem 1: Customer Demographics vs Purchase Behavior
-# Goal: Age aur gender ka purchase par impact
 
-# Histogram (Age distribution)
-# Bar chart (Gender vs Purchase)
-# Boxplot (Age vs Purchase)
+# Problem 1: Customer Demographics Impact on Purchase Behavior
+# Age Distibution (Histogram)
+# Gender vs Purchase (Bar Chart)
+
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 df = pd.read_csv(r"D:\games1\ecommerce_data.csv")
 
-# Age Distribution
 plt.figure()
-sns.histplot(df['age'], kde=True)
-plt.title("Age Distribution")
+plt.boxplot(df['age'])
+plt.title("Boxplot for Age (Outlier Detection)")
+plt.ylabel("Age")
 plt.show()
 
-# Gender vs Purchase
+Q1 = df['age'].quantile(0.25)
+Q3 = df['age'].quantile(0.75)
+IQR = Q3 - Q1
+
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+df_clean = df[(df['age'] >= lower_bound) & (df['age'] <= upper_bound)]
+
 plt.figure()
-sns.countplot(x='gender', hue='purchase', data=df)
-plt.title("Gender vs Purchase")
+plt.hist(df_clean['age'], bins=20)
+plt.title("Age Distribution (After Removing Outliers)")
+plt.xlabel("Age")
+plt.ylabel("Count")
 plt.show()
 
-# Age vs Purchase
+group = df_clean.groupby('gender')['purchase'].mean()
+
 plt.figure()
-sns.boxplot(x='purchase', y='age', data=df)
-plt.title("Age vs Purchase")
+group.plot(kind='bar')
+plt.title("Gender vs Purchase Rate (Cleaned Data)")
+plt.xlabel("Gender")
+plt.ylabel("Purchase Rate")
 plt.show()
